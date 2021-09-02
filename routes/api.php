b88799ddc21cbf44db68a69a0be78800457794ac
User <user@Users-Mac-mini.local>
2021-09-02 15:39:16 +0800

    Api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+Route::middleware('auth:api')->get('/token/revoke', function (Request $request) {
+    DB::table('oauth_access_tokens')
+        ->where('user_id', $request->user()->id)
+        ->update([
+            'revoked' => true
+        ]);
+    return response()->json('DONE');
 });
